99107cfdea19bc13d988d204f9f639f80757ce43
kodirov8788 <kodirov8788@gmail.com>
2023-08-23 09:06:43 +0500

    sadsa

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Issiq from './components/Issiq/Issiq';
 import Gazlik from './components/Gazlik/Gazlik';
 import Gazsiz from './components/Gazsiz/Gazsiz';
