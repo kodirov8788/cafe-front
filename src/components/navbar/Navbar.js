ed3ddac4dc55f10a5890823f79721ceda0bdee74
kodirov8788 <kodirov8788@gmail.com>
2023-08-23 09:06:45 +0500

    asdas

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import { ProductContext } from '../../context/ProductContext'
 import img1 from "../../image/shopping-cart.png"
 import { Link } from 'react-router-dom'
 function Navbar() {
-    const { cart, setCart } = useContext(ProductContext)
+    const { cart } = useContext(ProductContext)
     return (
         <div className='navbar'>
             <h1>CAFE</h1>
@@ -21,7 +21,7 @@ function Navbar() {
 
             <div className="navbar_cart">
                 <Link to="/order">zakazlar</Link>
-                <Link to="/madeorder">{"  "}Made order</Link>
+                <Link to="/madeorder">Made order</Link>
                 <Link to="/cart">
                     <img className='img1' src={img1} alt="" />
                     <span>{cart.length}</span>
